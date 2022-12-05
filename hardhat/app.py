620fc43b0251a8b850da08a26a221054cac100e0
Ivan Jevtić <ivanjevtic501@gmail.com>
2022-12-05 18:33:22 +0100

    change request interval
    infura too many requests

--- hardhat/app.py
+++ hardhat/app.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
   # serve(app, host="0.0.0.0", port=5000)
 
   scheduler = BackgroundScheduler()
-  scheduler.add_job(lambda: update_pending(), trigger="interval", seconds=15)
+  scheduler.add_job(lambda: update_pending(), trigger="interval", seconds=25)
   scheduler.start()
 
   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
